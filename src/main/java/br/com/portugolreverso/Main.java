7c42505b74a5cf2dbb0822fec2040b6e52aa51ae
Anthony Louis <anthony.physis@gmail.com>
2023-01-25 17:59:33 -0300

    feat: setup checkstyle plugin (#3)
    Setup the checkstyle plugin for the project using the config based on
    the Google Checks

---
+++ src/main/java/br/com/portugolreverso/Main.java
@@ -0,0 +1,21 @@
+package br.com.portugolreverso;
+
+public class Main {
+  public static void main(String... args) {
+    if (args.length != 1) {
+      throwErrorMessage();
+    }
+
+    System.out.println("Starting project!");
+  }
+
+  private static void throwErrorMessage() {
+    final var errorMessage =
+        """
+            Incorrect options while running command, you should run the follow command:
+                $ java -jar [PATH]/portugol-reverso-<VERSION>.jar [FILE-NAME]
+            """;
+
+    throw new RuntimeException(errorMessage);
+  }
+}
