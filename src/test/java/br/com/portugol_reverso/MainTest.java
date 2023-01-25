0bb1992c6a5b8e333de8153b3b6ea54792e943ed
Anthony Louis <anthony.physis@gmail.com>
2023-01-25 16:00:33 -0300

    feat: setup first classes for project

---
+++ src/test/java/br/com/portugol_reverso/MainTest.java
@@ -0,0 +1,13 @@
+package br.com.portugol_reverso;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class MainTest {
+
+    @Test
+    void passWrongCommandLine() {
+        Assertions.assertThrows(RuntimeException.class, () -> Main.main("arquivo1.txt", "arquivo2.txt"));
+    }
+}
